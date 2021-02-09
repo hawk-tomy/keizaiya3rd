569d35dd765544eafc459cec534b5a64874de6d9
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-02-10 08:24:54 +0900

    update user_manager.py

--- httpserver/user_manager.py
+++ httpserver/user_manager.py
@@ -10,6 +10,7 @@ class user():
         if len(kwargs) > i_l:
             raise ValueError('kwargs is longer than index')
         args += self._default[-(a_l - i_l):]
-        for k in kwargs:
+        for k,v in kwargs.items:
             if k in self._index:
                 i = self._index.index(k)
+                args[i] = v
